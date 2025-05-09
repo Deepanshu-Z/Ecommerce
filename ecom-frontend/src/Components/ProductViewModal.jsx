6e3d54be8092ea560b03806ac2bf20d5f08bf44f
Deepanshu Pokhriyal <deepanshupokhriyal07@gmail.com>
2025-05-09 16:59:38 +0530

    Defininig redux

--- ecom-frontend/src/Components/ProductViewModal.jsx
+++ ecom-frontend/src/Components/ProductViewModal.jsx
@@ -98,7 +98,7 @@ export default function ProductViewModal({
 
                 <div className="mt-4">
                   <Button
-                    className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10"
+                    className="cursor-pointer inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10"
                     onClick={handleClose}
                   >
                     CLOSE
